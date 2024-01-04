2bc8473273dc09ddfe2f521456836999ed730c20
tristanhannibal <tristanhannibal@gmail.com>
2024-01-04 16:26:40 -0600

    Add dragon popup

---
+++ src/components/DragonPopup.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+<template>
+    <img class="fix" src="/src/assets/dragon_face.png" alt="">
+</template>
+
+<style scoped>
+@media only screen and (max-width: 1600px) {
+  .fix {
+    display: none;
+  }
+}
+.fix{
+    position:fixed;
+    bottom:-291px;
+    left:50%;
+    transition: transform 1s;
+}
+.fix:hover{
+    transform: translateY(-260px);
+    transition: transform 1s;
+
+}
+</style>
